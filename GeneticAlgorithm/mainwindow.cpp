ffcc519e678c9592cd8aabcee8da1e29c6e9f874
sd <deryuzhovas@gmail.com>
2021-06-19 23:22:48 +0300

    Генетический алгоритм. Пока не очень стабильно

--- GeneticAlgorithm/mainwindow.cpp
+++ GeneticAlgorithm/mainwindow.cpp
@@ -12,22 +12,80 @@ using namespace std;
 vector<tuple<int, int>> points;
 vector<tuple<int, int>> startPoints;
 vector<tuple<int, int>> bestPoints;
-vector<vector<tuple<int, int>>> chromosomes;
+
 int pathCoast = INT_MAX;
 int bestPathCoast = INT_MAX ;
 double temp = 100;
 int countItems = 10;
-int countBurns = 0;
-
-struct Chromosome{
-    vector<tuple<int, int>> points;
-
-
-};
+int countBurns = 1;
 
 double distanceSqr(tuple<int, int> a, tuple<int, int> b) {
     return (std::get<0>(a) - std::get<0>(b)) * (std::get<0>(a) - std::get<0>(b)) + (std::get<1>(a) - std::get<1>(b)) * (std::get<1>(a) - std::get<1>(b));
 }
+
+struct Chromosome{
+
+
+
+    int getPathCoast() const
+    {
+        return pathCoast;
+    }
+
+    Chromosome(vector<tuple<int, int>> initPoints)
+    {
+        //std::vector<tuple<int, int>> points(initPoints);
+        points = initPoints;
+        pathCoast = 0;
+        for(int i=1;i<points.size();i++)
+        {
+            int x = std::get<0>(points[i]);
+            int y = std::get<1>(points[i]);
+            pathCoast+=distanceSqr(points[i], points[i-1]);
+        }
+        pathCoast+=distanceSqr(points[countItems-1], points[0]);
+    }
+
+    void RandomPlacement(int ind1, int ind2)
+    {
+        swap(points[ind1],points[ind2]);
+    }
+
+    void MutateByInversion(int numberOfStart, vector<tuple<int, int>>  newpoints)
+    {
+        //std::vector<tuple<int, int>> points(newpoints);
+        points = newpoints;
+        reverse( points.begin()+numberOfStart, points.end());
+        pathCoast = 0;
+        for(int i=1;i<points.size();i++)
+        {
+            int x = std::get<0>(points[i]);
+            int y = std::get<1>(points[i]);
+            pathCoast+=distanceSqr(points[i], points[i-1]);
+        }
+        pathCoast+=distanceSqr(points[countItems-1], points[0]);
+    }
+
+    int calcLength(){
+        int tmp = 0;
+        for(int i=1;i<points.size();i++)
+        {
+            int x = std::get<0>(points[i]);
+            int y = std::get<1>(points[i]);
+            tmp+=distanceSqr(points[i], points[i-1]);
+        }
+        tmp+=distanceSqr(points[countItems-1], points[0]);
+        return tmp;
+    }
+    vector<tuple<int, int>> points;
+    int pathCoast = INT_MAX;
+private:
+
+
+
+};
+
+vector<Chromosome> chromosomes;
 
 vector<tuple<int, int>> RandomPlacement(vector<tuple<int, int>> a, int n)
 {
@@ -58,7 +116,7 @@ MainWindow::MainWindow(QWidget *parent) :
     std::vector<tuple<int, int>> bestPoints(points);
     drawWithPath();
     bestPathCoast = pathCoast;
-    countBurns = 0;
+    countBurns = 1;
 }
 
 void MainWindow::drawWithPath()
@@ -114,22 +172,68 @@ void MainWindow::drawWithPath()
 
 void MainWindow::mutate()
 {
+    //chromosomes
+
+           // int worst = -1;
+         //   int worstVal = -1;
+         //   int i=0;
+             Chromosome *best = nullptr;
+             Chromosome *worst = nullptr;
+    for (Chromosome &chromosome : chromosomes) {
+            if(best==nullptr || best->pathCoast>=chromosome.pathCoast)
+                    best = &chromosome;
+            if(worst==nullptr || worst->pathCoast<=chromosome.pathCoast)
+                    worst = &chromosome;
+//            if(worst==-1 || worstVal>chromosome.getPathCoast())
+//            {
+//                worst = i;
+//                worstVal = chromosome.getPathCoast();
+//            }
+//                    best = &chromosome;
+//                    i++;
+          //  ui->textBrowser_4->insertPlainText(QString::number(chromosome.getPathCoast()));
+          //  ui->textBrowser_4->insertPlainText(",");
+
+    }
+ //ui->textBrowser_4->insertPlainText("                            ");
+    worst->MutateByInversion(countBurns,best->points);
+    //bestPathCoast =
+//    auto it = std::find(v.begin(), v.end(), 5);
+//    if(it != v.end())
+//        v.erase(it);
+    countBurns++;
+
 
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    chromosomes={};
-    chromosomes[0] = RandomPlacement(points, countItems);
-    chromosomes[1] = RandomPlacement(points, countItems);
-    chromosomes[2] = RandomPlacement(points, countItems);
-    while(temp>0.0000000001)
+    chromosomes.push_back(points);//Chromosome(RandomPlacement(points, countItems)));
+    chromosomes.push_back(Chromosome(RandomPlacement(points, countItems)));
+    chromosomes.push_back(Chromosome(RandomPlacement(points, countItems)));
+    while(countBurns<countItems-1)
     {
-
         mutate();
-//QThread::sleep(2);
-        //sleep(1000);
+        //temp=0.9*temp;
+        for (Chromosome &chromosome : chromosomes) {
+                ui->textBrowser_4->insertPlainText(QString::number(chromosome.getPathCoast()));
+                ui->textBrowser_4->insertPlainText(",");
+
+
+        }
+         ui->textBrowser_4->insertPlainText("                                    ");
     }
+    Chromosome *best = nullptr;
+    for (Chromosome &chromosome : chromosomes) {
+
+            if(best==nullptr || best->getPathCoast()>=chromosome.getPathCoast())
+                    best = &chromosome;
+    }
+
+    points = best->points;
+
+    drawWithPath();
+    countBurns = 1;
 }
 
 MainWindow::~MainWindow()
